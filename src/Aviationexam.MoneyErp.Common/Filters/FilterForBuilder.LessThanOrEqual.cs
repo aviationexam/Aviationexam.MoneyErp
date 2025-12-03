ea00cca5715a8117f5b532405d0acbce2abc24d0
Jan Trejbal <jan@trejbal.land>
2025-12-03 23:28:56 +0100

    Implement FilterForBuilder

--- src/Aviationexam.MoneyErp.Common/Filters/FilterForBuilder.LessThanOrEqual.cs
+++ src/Aviationexam.MoneyErp.Common/Filters/FilterForBuilder.LessThanOrEqual.cs
@@ -6,69 +6,51 @@ namespace Aviationexam.MoneyErp.Common.Filters;
 
 public readonly partial struct FilterForBuilder<T>
 {
-    public ReadOnlySpan<char> Equal(
-        Expression<Func<T, string>> property, string value
-    ) => FilterFor<T>.Equal(
-        property, value
-    );
-
-    public ReadOnlySpan<char> Equal<TP>(
+    public ReadOnlySpan<char> LessThanOrEqual<TP>(
         Expression<Func<T, TP>> property, TP value, string? format = null, IFormatProvider? provider = null
-    ) where TP : INumberBase<TP> => FilterFor<T>.Equal(
+    ) where TP : INumberBase<TP> => FilterFor<T>.LessThanOrEqual(
         property, value, format, provider
     );
 
-    public ReadOnlySpan<char> Equal<TP>(
+    public ReadOnlySpan<char> LessThanOrEqual<TP>(
         Expression<Func<T, TP?>> property, TP value, string? format = null, IFormatProvider? provider = null
-    ) where TP : struct, INumberBase<TP> => FilterFor<T>.Equal(
+    ) where TP : struct, INumberBase<TP> => FilterFor<T>.LessThanOrEqual(
         property, value, format, provider
     );
 
-    public ReadOnlySpan<char> Equal(
-        Expression<Func<T, bool>> property, bool value
-    ) => FilterFor<T>.Equal(
-        property, value
-    );
-
-    public ReadOnlySpan<char> Equal(
-        Expression<Func<T, bool?>> property, bool value
-    ) => FilterFor<T>.Equal(
-        property, value
-    );
-
-    public ReadOnlySpan<char> Equal(
+    public ReadOnlySpan<char> LessThanOrEqual(
         Expression<Func<T, DateTimeOffset>> property, DateOnly value
-    ) => FilterFor<T>.Equal(
+    ) => FilterFor<T>.LessThanOrEqual(
         property, value
     );
 
-    public ReadOnlySpan<char> Equal(
+    public ReadOnlySpan<char> LessThanOrEqual(
         Expression<Func<T, DateTimeOffset?>> property, DateOnly value
-    ) => FilterFor<T>.Equal(
+    ) => FilterFor<T>.LessThanOrEqual(
         property, value
     );
 
-    public ReadOnlySpan<char> Equal(
+    public ReadOnlySpan<char> LessThanOrEqual(
         Expression<Func<T, DateTime>> property, DateOnly value
-    ) => FilterFor<T>.Equal(
+    ) => FilterFor<T>.LessThanOrEqual(
         property, value
     );
 
-    public ReadOnlySpan<char> Equal(
+    public ReadOnlySpan<char> LessThanOrEqual(
         Expression<Func<T, DateTime?>> property, DateOnly value
-    ) => FilterFor<T>.Equal(
+    ) => FilterFor<T>.LessThanOrEqual(
         property, value
     );
 
-    public ReadOnlySpan<char> Equal(
+    public ReadOnlySpan<char> LessThanOrEqual(
         Expression<Func<T, DateOnly>> property, DateOnly value
-    ) => FilterFor<T>.Equal(
+    ) => FilterFor<T>.LessThanOrEqual(
         property, value
     );
 
-    public ReadOnlySpan<char> Equal(
+    public ReadOnlySpan<char> LessThanOrEqual(
         Expression<Func<T, DateOnly?>> property, DateOnly value
-    ) => FilterFor<T>.Equal(
+    ) => FilterFor<T>.LessThanOrEqual(
         property, value
     );
 }
