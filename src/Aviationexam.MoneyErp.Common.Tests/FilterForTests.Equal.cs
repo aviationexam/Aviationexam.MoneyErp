e9f3f2cf1ed7b279655fe9d18520bbc812dbb9cf
Jan Trejbal <jan@trejbal.land>
2025-12-03 22:20:10 +0100

    Add FilterForTests.Equal.cs

---
+++ src/Aviationexam.MoneyErp.Common.Tests/FilterForTests.Equal.cs
@@ -0,0 +1,72 @@
+using System;
+using Aviationexam.MoneyErp.Common.Filters;
+using Xunit;
+
+namespace Aviationexam.MoneyErp.Common.Tests;
+
+public partial class FilterForTests
+{
+    [Fact]
+    public void StringEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.AProperty, "AValue");
+
+        Assert.Equal("AProperty~eq~AValue", filter);
+    }
+
+    [Fact]
+    public void IntEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.IntProperty, 42);
+
+        Assert.Equal("IntProperty~eq~42", filter);
+    }
+
+    [Fact]
+    public void LongEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.LongProperty, 42);
+
+        Assert.Equal("LongProperty~eq~42", filter);
+    }
+
+    [Fact]
+    public void BoolEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.BoolProperty, true);
+
+        Assert.Equal("BoolProperty~eq~true", filter);
+    }
+
+    [Fact]
+    public void BoolEqualFalseWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.BoolProperty, false);
+
+        Assert.Equal("BoolProperty~eq~false", filter);
+    }
+
+    [Fact]
+    public void DateTimeOffsetEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.DateTimeOffsetProperty, new DateOnly(2023, 5, 15));
+
+        Assert.Equal("DateTimeOffsetProperty~eq~2023-05-15", filter);
+    }
+
+    [Fact]
+    public void DateTimeEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.DateTimeProperty, new DateOnly(2023, 5, 15));
+
+        Assert.Equal("DateTimeProperty~eq~2023-05-15", filter);
+    }
+
+    [Fact]
+    public void DateOnlyEqualWorks()
+    {
+        var filter = FilterFor<ApiModel>.Equal(x => x.DateOnlyProperty, new DateOnly(2023, 5, 15));
+
+        Assert.Equal("DateOnlyProperty~eq~2023-05-15", filter);
+    }
+}
--- src/Aviationexam.MoneyErp.Common.Tests/FilterForTests.cs
+++ src/Aviationexam.MoneyErp.Common.Tests/FilterForTests.cs
@@ -1,9 +1,10 @@
 using Aviationexam.MoneyErp.Common.Filters;
+using System;
 using Xunit;
 
 namespace Aviationexam.MoneyErp.Common.Tests;
 
-public class FilterForTests
+public partial class FilterForTests
 {
     [Fact]
     public void GetPropertyNameWorks()
@@ -16,5 +17,17 @@ public class FilterForTests
     private sealed class ApiModel
     {
         public required string AProperty { get; set; }
+
+        public int IntProperty { get; set; }
+
+        public int LongProperty { get; set; }
+
+        public bool BoolProperty { get; set; }
+
+        public DateTimeOffset DateTimeOffsetProperty { get; set; }
+
+        public DateTime DateTimeProperty { get; set; }
+
+        public DateOnly DateOnlyProperty { get; set; }
     }
 }
